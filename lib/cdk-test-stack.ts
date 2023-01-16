037caee46552656f146735a3b398294f4ccec127
Juan de Dios <juandedioz@gmail.com>
2023-01-15 21:25:58 -0500

    [fix] Update .gitignore

--- lib/cdk-test-stack.ts
+++ lib/cdk-test-stack.ts
@@ -1,34 +1,29 @@
 import * as cdk from "@aws-cdk/core";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
 import * as apigw from "@aws-cdk/aws-apigateway";
 import path = require("path");
-import { Cors } from "@aws-cdk/aws-apigateway";
 
 export class CdkTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
     const table = new dynamodb.Table(this, "Task", {
       partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
     });
 
     const saveTaskFunction = new lambda.Function(this, "DynamoLambdaHandler", {
       runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset(path.resolve(__dirname, "lambda")),
+      code: lambda.Code.fromAsset(path.resolve(__dirname, "lambdas")),
       handler: "handler.createTask",
       environment: {
         TASKS_TABLE_NAME: table.tableName,
       },
     });
 
-    // permissions to lambda to dynamo table
     table.grantReadWriteData(saveTaskFunction);
 
-    // create the API Gateway with one method and path
     const api = new apigw.RestApi(this, "task-api", {
       description: 'task-api',
     });
@@ -37,9 +32,5 @@ export class CdkTestStack extends cdk.Stack {
       .resourceForPath("task")
       .addMethod("POST", new apigw.LambdaIntegration(saveTaskFunction));
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
